b0e897f89b7c5a12c29cc9678104f4c1c39ba460
YunShu <im@yunshu.site>
2022-12-13 15:58:59 +0800

    Educational Codeforces Round 139 (Rated for Div. 2)

---
+++ codeforces/Educational Codeforces Round 139 (Rated for Div. 2)/A_Extremely_Round.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+constexpr int mxN = 999999;
+vector<int> nums;
+
+void solve(){
+    int n;
+    cin>>n;
+    int cnt = 0;
+    for(auto &i:nums){
+        if(i>n) break;
+        cnt++;
+    }
+    cout<<cnt<<"\n";
+}
+
+void fun(){
+    for(int i = 1;i<=mxN;++i){
+        string s = to_string(i);
+        if(s.size()-count(s.begin(), s.end(), '0')<=1){
+            nums.emplace_back(stoi(s));
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    fun();
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0; 
+}
