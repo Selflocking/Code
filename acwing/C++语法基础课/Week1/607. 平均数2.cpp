2f97b0d2e3caa24494d46190f8cfd6866f8b8bd1
YunShu <im@yunshu.site>
2024-02-18 19:17:39 +0800

    Acwing: 607. 平均数2

---
+++ acwing/C++语法基础课/Week1/607. 平均数2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    double res = (a * 2 + b * 3 + c * 5) / 10;
+    printf("MEDIA = %.1f", res);
+    return 0;
+}
