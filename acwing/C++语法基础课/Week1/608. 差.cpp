42cace22c11a798ef03f65394f5fba9759bd6ad3
YunShu <im@yunshu.site>
2024-02-18 19:21:47 +0800

    Acwing: 608. 差

---
+++ acwing/C++语法基础课/Week1/608. 差.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int res = a * b - c * d;
+    cout << "DIFERENCA = " << res << endl;
+    return 0;
+}
